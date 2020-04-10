d70e5c7efcc285981f09da5015c30820a33a0d40
Marcelo Fernandez <marceloleandrofernandez@gmail.com>
2020-04-10 15:36:16 -0300

    Update SQLQuery_1.sql

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -3,3 +3,7 @@ SELECT * from autores
 go
 --exec sp_help 'autores'
 --asdfasfasdfasdfsadf
+
+SELECT empleados.*,cargos.cargo_descripcion
+FROM empleados,cargos
+WHERE empleados.cargo_id= cargos.cargo_id
